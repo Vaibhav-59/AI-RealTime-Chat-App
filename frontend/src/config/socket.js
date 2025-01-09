7d3cf2f50b21faafbeed9827c61e3da47cbff88d
Vaibhav-59 <162326575+Vaibhav-59@users.noreply.github.com>
2025-01-09 22:33:20 +0530

    Update socket.js

--- frontend/src/config/socket.js
+++ frontend/src/config/socket.js
@@ -5,7 +5,7 @@ let socketInstance = null;
 
 export const initializeSocket = (projectId) => {
 
-    socketInstance = socket( import.meta.env.VITE_API_URL, {
+    socketInstance = socket( 'https://chat-app-backend-paex.onrender.com', {
         auth: {
             token: localStorage.getItem('token')
         },
